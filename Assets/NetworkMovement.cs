bac1aa4a09877402e629561c175bd23c6c014cf3
churchmf <churchmf@gmail.com>
2015-03-14 13:01:18 -0600

    better movement

--- Assets/NetworkMovement.cs
+++ Assets/NetworkMovement.cs
@@ -10,34 +10,37 @@ public class NetworkMovement : MonoBehaviour
 		public int moveSpeed = 8;
 		public bool haveControl = false;
 		public float JumpThreshold = 0.1f;
+		public float JumpForce = 5;
 
 		void FixedUpdate ()
 		{
 				if (haveControl) {
-						float vert = Input.GetAxis ("Vertical");
 						float horiz = Input.GetAxis ("Horizontal");
-
 						bool wantsToJump = Input.GetButtonDown ("Jump");
-						bool inAir = (transform.position.y > JumpThreshold);
 
-						MovePlayer (vert, horiz);
+						MovePlayer (horiz, wantsToJump);
 						if (Network.isClient) {
-								networkView.RPC ("MovePlayer", RPCMode.Server, vert, horiz);
+								networkView.RPC ("MovePlayer", RPCMode.Server, horiz, wantsToJump);
 						}
 				}
 		}
 
 		[RPC]
-		void MovePlayer (float vert, float horiz)
+		void MovePlayer (float horiz, bool wantsToJump)
 		{
-				Vector3 newVelocity = (transform.right * horiz * moveSpeed) + (transform.forward * vert * moveSpeed);
+				Vector3 newVelocity = (transform.forward * -horiz * moveSpeed); // + (transform.forward * vert * moveSpeed);
 				Vector3 myVelocity = rigidbody.velocity;
 				myVelocity.x = newVelocity.x;
 				myVelocity.z = newVelocity.z;
 
+				bool inAir = (transform.position.y > JumpThreshold);
+				if (wantsToJump && !inAir) {
+						rigidbody.AddForce (0, JumpForce, 0);
+				}
+
 				rigidbody.AddTorque (myVelocity);
 
 				// Lock Z axis
-				transform.position = new Vector3 (transform.position.x, transform.position.y, 0);
+				//transform.position = new Vector3 (transform.position.x, transform.position.y, 0);
 		}
 }
